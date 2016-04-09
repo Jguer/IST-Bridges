101ca39e37d2ebec4cf7aff32993058cbe6a95e0
Jguer <joaogg3@gmail.com>
2016-04-09 23:56:38 +0100

    Removed debug code

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /**************************************
 * Bridges @ AED-Project 2016
-* by 
+* by
 * João Guerreiro & Mariana Martins
-* 
+*
 * main.c
 ***************************************/
 #include <stdio.h>
@@ -14,20 +14,19 @@
 
 int main(int argc, char **argv)
 {
-	FILE *map_file;
-	int lineData[4];
+    FILE *map_file;
+    int lineData[4];
 
-	if(argc != 2 || strcmp(get_filename_ext(argv[1]), "map"))
-		file_error("Missing arguments or wrong extension specified on file input");
+    if(argc != 2 || strcmp(get_filename_ext(argv[1]), "map"))
+        file_error("Missing arguments or wrong extension specified on file input");
 
-	map_file = fopen(argv[1], "r");
-	if(map_file == NULL)
-		file_error("Unable to open file specified");
+    map_file = fopen(argv[1], "r");
+    if(map_file == NULL)
+        file_error("Unable to open file specified");
 
-	read_line(map_file, lineData);
-	assign_values(lineData, &line, &col, &n_ilhas, &var);
+    read_line(map_file, lineData);
 
-	fclose(map_file); /* -will be closed when end is reached. also when return is 0. to define later! */
+    fclose(map_file); /* -will be closed when end is reached. also when return is 0. to define later! */
 
     return 0;
 }
