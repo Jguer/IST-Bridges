d79adf90bc8dd023fa724a44a1c5fa3146b3f39f
Jguer <joaogg3@gmail.com>
2016-04-10 01:11:04 +0100

    Fixed includes

--- filereader.c
+++ filereader.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "filereader.h"
 
 /* Reads the integers from one line of the maps file
@@ -7,34 +5,34 @@
  */
 int read_line(FILE* map_file, int *lineData)
 {
-	int scanfresult;
-	int i = 0;
+    int scanfresult;
+    int i = 0;
 
-	while(i<4)
-	{
-		scanfresult = fscanf(map_file, "%d", &lineData[i]);
+    while(i<4)
+    {
+        scanfresult = fscanf(map_file, "%d", &lineData[i]);
 
-		/* if end of file: 0 */
-		if(scanfresult == EOF)
-			return 0;
-		/* if end of map: -1 */
-		else if(lineData[i] == -1)
-			return -1;
+        /* if end of file: 0 */
+        if(scanfresult == EOF)
+            return 0;
+        /* if end of map: -1 */
+        else if(lineData[i] == -1)
+            return -1;
 
-		i++;
-	}
+        i++;
+    }
 
-	return 1;
+    return 1;
 }
 
 /* Assigns lineData[] values to any other important values */
 void assign_values(int *linedata, int *val0, int *val1, int *val2, int *val3)
 {
-	*val0 = linedata[0];
-	*val1 = linedata[1];
-	*val2 = linedata[2];
-	*val3 = linedata[3];
-	return;
+    *val0 = linedata[0];
+    *val1 = linedata[1];
+    *val2 = linedata[2];
+    *val3 = linedata[3];
+    return;
 }
 
 
--- filereader.h
+++ filereader.h
@@ -1,6 +1,9 @@
 #ifndef FILEREADER_HEADER
 #define FILEREADER_HEADER
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int read_line(FILE* map_file, int *lineData);
 void assign_values(int *linedata, int *val0, int *val1, int *val2, int *val3);
 
