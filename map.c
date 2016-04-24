896388917e7cea66f3fb9aa6bde7c11ed749615a
Jguer <joaogg3@gmail.com>
2016-04-24 12:12:17 +0100

    Added new and improved map structure

--- map.c
+++ map.c
@@ -1,64 +1,54 @@
 #include "map.h"
+#include "isla.h"
 
 struct _map {
-
-	int nlines;
-	int ncols;
-	int nislas;
-	int typevar;
+    isla ***tile;
+    int x_max;
+    int y_max;
+    char map_mode;
 };
 
-map *create_map()
+map *create_map(int x_max, int y_max, int map_mode)
 {
+    int i;
     map *new_map;
-    new_map = (map *)malloc(sizeof(map));
-    if(new_map == NULL)
-    	memory_error("Unable to create structure map");
 
-    new_map->nlines = 0;
-    new_map->ncols = 0;
-    new_map->nislas = 0;
-    new_map->typevar = 0;
+    new_map = (map *)malloc(sizeof(map));
+    if( NULL == new_map ) {
+        memory_error("Unable to allocate map");
+    }
+
+    new_map->x_max= x_max;
+    new_map->y_max= y_max;
+
+    /* Allocate x axis so we can have a nice tile[x] */
+    new_map->tile = (isla ***) calloc(x_max , sizeof(isla**));
+    if( NULL == new_map->tile)
+    {
+        memory_error("Unable to allocae x axis of map");
+    }
+    /* Allocate y axis so we can have a nice tile[x] */
+    for ( i = 0; i < x_max; i++)
+    {
+        new_map->tile[i] = (isla **) calloc( y_max , sizeof(isla*));
+        if( NULL == new_map->tile[i] )
+        {
+            memory_error("Unable to allocate y axis of map");
+        }
+    }
+
+
 
     return new_map;
 }
 
-map* assign_to_map(map* new_map, int* linedata)
+isla *get_tile(map *got_map, int x, int y)
 {
-	assign_values(linedata, &new_map->nlines, &new_map->ncols, &new_map->nislas, &new_map->typevar, NULL);
-	return new_map;
+    return got_map->tile[x][y];
 }
 
-int get_nlines(map* new_map)
+void set_tile(map *got_map, isla *isla_to_add)
 {
-  return new_map->nlines;
-}
-int get_ncols(map* new_map)
-{
-  return new_map->ncols;
-}
-int get_nislas(map* new_map)
-{
-  return new_map->nislas;
-}
-int get_typevar(map* new_map)
-{
-  return new_map->typevar;
-}
-
-void print_map(map* new_map)
-{
-	fprintf(DEBUG_LOC,
-            KGRN "nlines:" RESET " %d "
-            KGRN "ncols:" RESET " %d "
-            KGRN "nislas:" RESET " %d " 
-            KGRN "variante:" RESET " %d\n",
-            new_map->nlines, new_map->ncols, new_map->nislas, new_map->typevar);
+    got_map->tile[get_x(get_pos_isla(isla_to_add))][ get_y(get_pos_isla(isla_to_add))] = isla_to_add;
     return;
-}
-
-void free_map(map* new_map)
-{
-  free(new_map);
-  return;
 }
--- map.h
+++ map.h
@@ -3,13 +3,5 @@
 #include "defs.h"
 
 typedef struct _map map;
-map *create_map();
-map* assign_to_map(map* new_map, int* linedata);
-int get_nlines(map* new_map);
-int get_ncols(map* new_map);
-int get_nislas(map* new_map);
-int get_typevar(map* new_map);
-void print_map(map* new_map);
-void free_map(map* new_map);
 
 #endif
