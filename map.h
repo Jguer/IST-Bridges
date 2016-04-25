10eb2f8ec0cb93cfd6ee5277d6ed280e5f9acabc
Mrrvm <mrrvm@hotmail.com>
2016-04-25 16:27:50 +0100

    map.c
    added function to h, changed includes

--- map.h
+++ map.h
@@ -1,7 +1,8 @@
 #ifndef MAP_HEADER
 #define MAP_HEADER
-#include "defs.h"
+#include "isla.h"
 
 typedef struct _map map;
-
+map *create_map(int x_max, int y_max, int n_bridges, int map_mode);
+void set_tile(map *got_map, isla* isla_to_add);
 #endif
