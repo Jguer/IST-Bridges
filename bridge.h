2e14252e4f6935214cbe7d07c03933ef378a63a2
Mrrvm <mrrvm@hotmail.com>
2016-04-26 11:42:56 +0100

    added non functions find adjacent fucntions

--- bridge.h
+++ bridge.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "file.h"
+
 int randomize(int limit);
+
+void find_adj(map* got_map);
+void find_adj_y(isla* active_row_node, map *got_map);
+isla* find_next_isla_y(map *got_map, int static_x, int y, int y_max);
+isla* find_next_isla_x(map *got_map, int x, int static_y, int x_max);
 
 #endif
