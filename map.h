33f51737d2df92f00dbfa993e749abf2d2571249
Mrrvm <mrrvm@hotmail.com>
2016-04-26 11:43:35 +0100

    added get_tile to h

--- map.h
+++ map.h
@@ -4,6 +4,7 @@
 
 typedef struct _map map;
 map *create_map(int x_max, int y_max, int n_bridges, int map_mode);
+isla *get_tile(map *got_map, int x, int y);
 void set_tile(map *got_map, isla* isla_to_add);
 int get_n_bridges(map* got_map);
 int get_x_max(map* got_map);
