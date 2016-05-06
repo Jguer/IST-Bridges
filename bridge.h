d87ae50e63a5e11562d6f1ffce270a9d2dfcc975
Mrrvm <mrrvm@hotmail.com>
2016-05-07 00:47:11 +0100

    added print adj

--- bridge.h
+++ bridge.h
@@ -11,5 +11,6 @@ void find_adj(map* got_map);
 void find_adj_y(isla* active_row_node, map *got_map);
 isla* find_next_isla_y(map *got_map, int static_x, int y, int y_max);
 isla* find_next_isla_x(map *got_map, int x, int static_y, int x_max);
+void print_adj(list* isla_list);
 
 #endif
