79d270d0ed051dfd3568c5f0585795418210aeaf
Mrrvm <mrrvm@hotmail.com>
2016-05-15 20:15:28 +0100

    added gen_dynamic

--- search_engine.c
+++ search_engine.c
@@ -191,6 +191,108 @@ bool is_connectable(isla *isla_a, isla *isla_b, int adj_index, stack *got_stack,
     return FALSE;
 }
 
+int gen_essential_bridges(list *isla_list, stack *got_stack, list* probi_list)
+{
+    int n_bridges, n_adj;
+    int dir = 0;
+    bridge *new_bridge = NULL;
+    node *new_node = NULL;
+    isla *new_isla = NULL, *_adj = NULL;
+
+    new_node = get_head(isla_list);
+
+    while(new_node != NULL)
+    {
+        new_isla = get_node_item(new_node);
+        n_adj = get_adj_number(new_isla);
+        n_bridges = get_isla_bridges_avb(new_isla);
+
+
+        if(n_adj < n_bridges)
+        {
+            for(dir=0; dir<4; dir++)
+            {
+                _adj = get_isla_adj(new_isla, dir);
+                if(_adj != NULL)
+                {
+#ifdef DEBUG
+                    printf("Isla1: %d Isla2: %d\n", get_isla_name(new_isla), get_isla_name(_adj));
+#endif
+                    if(is_connectable(new_isla, _adj, dir, got_stack, probi_list) == TRUE  &&
+                        is_connected(new_isla, dir) == FALSE && is_connected(_adj, get_opposite_dir(dir)) == FALSE)
+                    {
+                        new_bridge = connect_islas(new_isla, _adj, dir);
+                        push_to_stack(got_stack, (item)new_bridge);
+                    }
+                }
+            }
+        }
+
+        new_node = get_next_node(new_node);
+    }
+
+    return (int) get_stack_size(got_stack);
+}
+
+int gen_dynamic_obivous_bridges(list *isla_list, stack *got_stack, list* probi_list, map *got_map)
+{
+    int n_bridges, n_adj;
+    int dir = 0;
+    bridge *new_bridge = NULL;
+    node *new_node = NULL;
+    isla *new_isla = NULL, *_adj = NULL;
+    bool is_solved = FALSE;
+    int mode = get_map_mode(got_map);
+
+    new_node = get_head(isla_list);
+
+    while(new_node != NULL)
+    {
+        new_isla = get_node_item(new_node);
+        n_adj = get_adj_number(new_isla);
+        n_bridges = get_isla_bridge_s_avb(new_isla);
+
+
+        if(n_adj < n_bridges)
+        {
+            for(dir=0; dir<4; dir++)
+            {
+                _adj = get_isla_adj(new_isla, dir);
+                if(_adj != NULL)
+                {
+#ifdef DEBUG
+                    printf("Isla1: %d Isla2: %d\n", get_isla_name(new_isla), get_isla_name(_adj));
+#endif
+                    if(is_connectable(new_isla, _adj, dir, got_stack, probi_list) == TRUE  &&
+                        is_connected(new_isla, dir) == FALSE && is_connected(_adj, get_opposite_dir(dir)) == FALSE)
+                    {
+                        new_bridge = connect_islas(new_isla, _adj, dir);
+                        push_to_stack(got_stack, (item)new_bridge);
+#ifdef DEBUG
+                        printf("Connect %d - %d\n", get_isla_name(new_isla), get_isla_name(_adj));
+#endif
+                    }
+                }
+            }
+        }
+
+        new_node = get_next_node(new_node);
+    }
+
+    is_solved = check_for_allzero(isla_list);
+    if(mode == 1 || mode == 2)
+        return is_solved;
+
+    else
+    {
+        if(is_solved)
+            return check_for_allconnected(isla_list);
+        else
+            return is_solved;
+
+    }
+}
+
 void DFS_engine(isla *edgy, bool *visited, map* got_map, stack *bridge_stack, list *probi_list)
 {
     isla *_adj = NULL;
@@ -343,7 +445,7 @@ int backtrack(stack *got_stack, list *isla_list, map *got_map, int obvious)
 
         dfs_counter ++;
 
-        if((int) get_stack_size(got_stack) >= obvious && is_solved == FALSE)
+        if((int) get_stack_size(got_stack) >= obvious && is_solved == FALSE && get_next_node(get_stack_head(got_stack))!= NULL)
         {
             last_bridge = get_node_item(get_next_node(get_stack_head(got_stack)));
         }
@@ -363,50 +465,16 @@ int backtrack(stack *got_stack, list *isla_list, map *got_map, int obvious)
 }
 
 
-int gen_essential_bridges(list *isla_list, stack *got_stack)
-{
-    int n_bridges, n_adj;
-    int dir = 0;
-    bridge *new_bridge = NULL;
-    node *new_node = NULL;
-    isla *new_isla = NULL, *_adj = NULL;
-
-    new_node = get_head(isla_list);
-
-    while(new_node != NULL)
-    {
-        new_isla = get_node_item(new_node);
-        n_adj = get_adj_number(new_isla);
-        n_bridges = get_isla_bridges_avb(new_isla);
-
-        if(n_adj < n_bridges)
-        {
-            for(dir=0; dir<4; dir++)
-            {
-                _adj = get_isla_adj(new_isla, dir);
-                if(_adj != NULL)
-                {
-                    if(is_connected(new_isla, dir) == FALSE && is_connected(_adj, get_opposite_dir(dir)) == FALSE)
-                    {
-                        new_bridge = connect_islas(new_isla, _adj, dir);
-                        push_to_stack(got_stack, (item)new_bridge);
-                    }
-                }
-            }
-        }
-
-        new_node = get_next_node(new_node);
-    }
-
-    return (int) get_stack_size(got_stack);
-}
 
 stack *DFS_manager(list *isla_list, map* got_map)
 {
     int   obv_gen      = 0;
     stack *new_stack   = create_stack();
 
-    obv_gen = gen_essential_bridges(isla_list, new_stack);
+    obv_gen = gen_essential_bridges(isla_list, new_stack, NULL);
+#ifdef DEBUG
+    print_stack(new_stack, print_bridge);
+#endif
     sort_list(isla_list, is_isla_greater_avb);
 #ifdef DEBUG
     print_list(isla_list, print_isla);
